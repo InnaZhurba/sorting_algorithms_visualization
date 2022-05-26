ad01c731a0ddf35ae218cce08afb589882efb683
Bohdan <bodya.ru02@gmail.com>
2022-05-26 14:39:03 +0300

    Add animation, though I have no idea how to use it while sorting :(

--- src/components/AlgoDemo.js
+++ src/components/AlgoDemo.js
@@ -1,18 +1,19 @@
-import { IconButton, Slider } from "@mui/material";
+import { duration, IconButton, Slider } from "@mui/material";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 import "../styles/AlgoDemo.css";
 import React, { useContext, useState } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const CurrentArrayContext = React.createContext({currentArray: [], setCurrentArray: () => {}});
 const ArraySizeContext = React.createContext({arraySize: [], setArraySize: () => {}});
 const CurrentInputContext = React.createContext({currentInput: [], setCurrentInput: () => {}});
 
-
+//  Main component
 const AlgoDemo = () => {
     const [currentArray, setCurrentArray] = useState([]);
-    const [arraySize, setArraySize] = useState(0);
+    const [arraySize, setArraySize] = useState(5);
     const [currentInput, setCurrentInput] = useState('');
     
     return <div className="algo_demo">
@@ -57,7 +58,7 @@ const ArrayGenerator = () => {
     return <div className="size_generation">
         <div className="array_size">
             <label className="algo_description">Array size: </label>
-            <input className="size_input" type="text" id="size-input" onChange={(ev) => {
+            <input className="size_input" value={arraySize} type="text" id="size-input" onChange={(ev) => {
                 const parsedInt = parseInt(ev.target.value);
                 setArraySize(isNaN(parsedInt) ? 0 : parsedInt);
                 }} />
@@ -88,9 +89,48 @@ const Box = ({value}) => {
 
 const BoxContainer = () => {
     const {currentArray} = useContext(CurrentArrayContext);
+    const index1 = 0;
+    const index2 = 4;
+
+    let animationForElementsByIndex = getSwapAnimation(index1, index2);
+
     return <div className="elements_visualization">
-        {currentArray.map((el, index) => {return <Box value={el} key={index}/>})}
+        {currentArray.map((el, index) => {
+            if (animationForElementsByIndex[index]) {
+                const {x, y} = animationForElementsByIndex[index];
+                return <motion.div animate = {{
+                    x: x,
+                    y: y,
+                }}>
+                    <Box value={el} key={index}/>
+                </motion.div>
+            }
+            else {
+                return <Box value={el} key={index}/>
+            }
+            
+            })}
     </div>
+}
+
+
+const getSwapAnimation = (index1, index2) => {
+    const boxWidth = 50;
+    const boxMargin = 20;
+    const xdir_index1 = (index1 > index2 ? -1 : 1)*Math.abs(index1-index2)*(boxWidth+2*boxMargin);
+    const xdir_index2 = -xdir_index1;
+
+    let animationForElementsByIndex = {};
+    animationForElementsByIndex[index1] = {
+        x: [0, 0, xdir_index1, xdir_index1],
+        y: [0, -100, -100, 0]
+    };
+    animationForElementsByIndex[index2] = {
+        x: [0, 0, xdir_index2, xdir_index2],
+        y: [0, 100, 100, 0]
+    };
+
+    return animationForElementsByIndex;
 }
 
 export default AlgoDemo;
